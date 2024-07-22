056f3b3b396e61f5daaf380cceeeb7a1cd0402f8
DaisyAram <daisyarama03@gmail.com>
2024-07-22 10:03:45 +0000

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,16 +1,17 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-let count = 0;
-function updateCounter() {
-	count += 1;
-	$('count').text(`${count} clicks on the button`);
-}
-
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id ="count"></p>');
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('#myButton').click(_.debounce(updateCounter, 300));
+let conunt = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,24 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
 };
