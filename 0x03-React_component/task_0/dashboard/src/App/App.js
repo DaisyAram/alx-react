8eb803dd504309154a835fbb590a7425b6d86672
DaisyAram <daisyarama03@gmail.com>
2024-07-30 08:36:39 +0000

    Commence with class components

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -8,22 +8,20 @@ import PropTypes from "prop-types";
 import "./App.css";
 import { getLatestNotification } from "../utils/utils";
 
-const listCourses = [
-  { id: 1, name: "ES6", credit: 60 },
-  { id: 2, name: "Webpack", credit: 20 },
-  { id: 3, name: "React", credit: 40 },
-];
-
-const listNotifications = [
-  { id: 1, type: "default", value: "New course available" },
-  { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: getLatestNotification() },
-];
-
 class App extends Component {
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
   render() {
-    const { isLoggedIn } = this.props;
-    
     return (
       <React.Fragment>
         <div className="App">
