53617c7d028510cdba1b54c26202d1d8f0210323
DaisyAram <daisyarama03@gmail.com>
2024-08-13 08:10:32 +0000

    utils.test

--- 0x01-react_intro/task_4/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_4/dashboard/src/utils.test.js
@@ -7,19 +7,16 @@ describe("Get full year", function() {
   });
 })
 
-describe("Get footer message", function() {
-  const trueMessage = 'Holberton School';
-  const falseMessage = 'Holberton School main dashboard';
 
-  it("should return the true footer message", function() {
-    const currentMessage = getFooterCopy(true);
-    expect(currentMessage).toBe(trueMessage);
-  });
-  it("should return the false footer message", function() {
-    const currentMessage = getFooterCopy(false);
-    expect(currentMessage).toBe(falseMessage);
-  })
-})
+describe("Get footer copy", function() {
+    it('returns "Holberton School" when isIndex is true', function() {
+        expect(getFooterCopy(true)).toBe('Holberton School');
+    });
+
+    it('returns "Holberton School main dashboard" when isIndex is false', function() {
+        expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+    });
+});
 
 describe("Get latest notification", function() {
   it("should return the urgent message text", function() {
