fb2f4bcaf1b2e5fd3f5cc9c683cb5164e15a5cdb
DaisyAram <daisyarama03@gmail.com>
2024-08-20 08:36:15 +0000

    read data from a json

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,7 +1,5 @@
 import * as notificationData from "../../../../notifications.json";
 
-function getAllNotificationsByUser(userId) {
+export default function getAllNotificationsByUser(userId) {
   return notificationData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
 }
-
-export default getAllNotificationsByUse
