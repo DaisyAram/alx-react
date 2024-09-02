a405674f6796024a6eacb36baf023dc8bf3b9cbc
DaisyAram <daisyarama03@gmail.com>
2024-09-02 13:38:27 +0000

    test

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/reducers/uiReducer.js
@@ -1,29 +1,41 @@
-import immutable from 'immutable';
-import { LOGOUT, LOGIN, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from './uiActionTypes';
+import { Map } from "immutable";
 
-const { Map } = immutable;
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from "../actions/uiActionTypes";
 
 export const initialState = {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {}
-}
-
-export const  uiReducer = (state = initialState, action) => {
-	state = Map(state);
-    switch (action.type) {
-        case DISPLAY_NOTIFICATION_DRAWER:
-            return { ...state, isNotificationDrawerVisible: true}
-        case HIDE_NOTIFICATION_DRAWER:
-            return { ...state, isNotificationDrawerVisible: false}
-        case LOGIN_SUCCESS:
-            return { ...state, isUserLoggedIn: true}
-        case LOGIN_FAILURE:
-            return { ...state, isUserLoggedIn: false}
-        case LOGOUT:
-            return { ...state, isUserLoggedIn: false}
-        default:
-            break;
-    }
-    return state;
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {},
 };
+
+const uiReducer = (state = Map(initialState), action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", true);
+
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", false);
+
+    case LOGIN_SUCCESS:
+      return state.set("isUserLoggedIn", true);
+
+    case LOGIN_FAILURE:
+      return state.set("isUserLoggedIn", false);
+
+    case LOGOUT:
+      return state.set("isUserLoggedIn", false);
+
+    default:
+      break;
+  }
+  return state;
+};
+
+export default uiReducer;
